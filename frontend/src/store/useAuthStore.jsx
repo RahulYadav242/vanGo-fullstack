8ddfc3b66cf0352deb2eebe8aa7d6f2d6e60ffb4
16111026 <68323930+16111026@users.noreply.github.com>
2025-06-22 15:49:17 +0530

    Update useAuthStore.jsx

--- frontend/src/store/useAuthStore.jsx
+++ frontend/src/store/useAuthStore.jsx
@@ -7,10 +7,6 @@ const BASE_URL = import.meta.env.MODE === "development"
   ? "http://localhost:5001"
   : "https://savo.onrender.com";
 
-const BASE_URL = import.meta.env.MODE === "development"
-  ? "http://localhost:5001"
-  : "https://savo.onrender.com";
-
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
